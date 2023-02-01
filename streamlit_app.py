edb2fb191841cbb1a206182d28375800671be2d4
clifflepage <123758123+clifflepage@users.noreply.github.com>
2023-01-31 19:04:33 -0500

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,8 +61,7 @@ if streamlit.button('Get Fruit List'):
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("INSERT INTO fruit_load_list (fruit_name) SELECT '" + new_fruit + "' AS fruit_name
-WHERE NOT EXISTS (SELECT * FROM fruit_load_list WHERE fruit_name = '" + new_fruit + "')")
+        my_cur.execute("INSERT INTO fruit_load_list (fruit_name) SELECT '" + new_fruit + "' AS fruit_name WHERE NOT EXISTS (SELECT * FROM fruit_load_list WHERE fruit_name = '" + new_fruit + "')")
         return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
